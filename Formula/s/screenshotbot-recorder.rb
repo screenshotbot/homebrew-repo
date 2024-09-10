bd635380060af51e4e70d63836c633022fabed35
Arnold Noronha <arnold@tdrhq.com>
2024-09-10 14:00:43 -0400

    more testing

--- Formula/s/screenshotbot-recorder.rb
+++ Formula/s/screenshotbot-recorder.rb
@@ -5,7 +5,7 @@ class ScreenshotbotRecorder < Formula
   sha256 "6c7ba5e167bf34e413b1a1738e2b4da9f38228bf8688ea3b1f40e4f8d8735853"
 
   def install
-    echo "hello world"
+    system "ls"
   end
 
 
