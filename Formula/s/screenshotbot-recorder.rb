5b618980127f038c4f9cfb14df8da2fd71ac6277
Arnold Noronha <arnold@tdrhq.com>
2024-09-10 14:36:54 -0400

    use new archive

--- Formula/s/screenshotbot-recorder.rb
+++ Formula/s/screenshotbot-recorder.rb
@@ -1,7 +1,7 @@
 class ScreenshotbotRecorder < Formula
   desc "The Screenshotbot Recorder tool"
   homepage "https://screenshotbot.io"
-  url "https://cdn.screenshotbot.io/artifact/releases/2.8.9/recorder-darwin"
+  url "https://cdn.screenshotbot.io/artifact/releases/2.8.9/recorder-homebrew.tar.gz"
   sha256 "6c7ba5e167bf34e413b1a1738e2b4da9f38228bf8688ea3b1f40e4f8d8735853"
 
   def install
