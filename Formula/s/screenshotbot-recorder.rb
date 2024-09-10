540822f38f7f97bf5fe2b9e9a0641930c9548cc7
Arnold Noronha <arnold@tdrhq.com>
2024-09-10 13:57:55 -0400

    first formula

---
+++ Formula/s/screenshotbot-recorder.rb
@@ -0,0 +1,12 @@
+class ScreenshotbotRecorder < Formula
+  desc "The Screenshotbot Recorder tool"
+  homepage "https://screenshotbot.io"
+  url "https://cdn.screenshotbot.io/artifact/releases/2.8.9/recorder-darwin"
+  sha256 "6c7ba5e167bf34e413b1a1738e2b4da9f38228bf8688ea3b1f40e4f8d8735853"
+
+  def install
+    echo "hello world"
+  end
+
+
+end
