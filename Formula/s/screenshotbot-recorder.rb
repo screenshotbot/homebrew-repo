7911c322547e125b0cf698827c90aa468a315d70
Arnold Noronha <arnold@tdrhq.com>
2024-09-16 16:32:35 -0400

    Bump version 2.8.13

--- Formula/s/screenshotbot-recorder.rb
+++ Formula/s/screenshotbot-recorder.rb
@@ -1,8 +1,8 @@
 class ScreenshotbotRecorder < Formula
   desc "The Screenshotbot Recorder tool"
   homepage "https://screenshotbot.io"
-  url "https://cdn.screenshotbot.io/artifact/releases/2.8.9/screenshotbot-homebrew.tar.gz"
-  sha256 "7fcd8fbf9b0852317c9764c7287b89613b6b9b90add41056a5412e46e89e6086"
+  url "https://cdn.screenshotbot.io/artifact/releases/2.8.13/screenshotbot-homebrew-7c395a4852844f1e0c73dbac49269446c1ff66485f594bf6476529cf85979ab9.tar.gz"
+  sha256 "7c395a4852844f1e0c73dbac49269446c1ff66485f594bf6476529cf85979ab9"
 
   def install
     system "mkdir", "-p",bin
